918a64a442c2e77f6e7aa3f957f758ca9738cb8c
Peter Liu <hyliu.amoiensis@gmail.com>
2025-05-14 00:17:00 +1200

    bugfix

--- forum/user/views.py
+++ forum/user/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
