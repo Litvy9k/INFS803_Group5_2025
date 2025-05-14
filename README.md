228af80a66bd7488abc9a48ca95509bbbffa4f24
Peter Liu <hyliu.amoiensis@gmail.com>
2025-05-14 13:21:02 +1200

    logout

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ hello
 not really a readme just seems more like it this way bruh
 
 Functional backend apis: 
+
 /api/user/register/
+
 /api/user/login/
+
 /api/user/login/refresh/ - for getting new token up to 7 days
+
+/api/user/logout/ - need do provide the refresh token
 
 also there will be a 405 error in DRF visualized apis but it shouldn't be a problem for frontend
--- forum/user/views.py
+++ forum/user/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -13,3 +17,18 @@ class RegisterView(generics.CreateAPIView):
         user = serializer.save()
         data = serializer.data
         return Response(data, status=status.HTTP_201_CREATED)
+    
+class LogoutView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return Response({"detail": "No refresh token provided"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response({"detail": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
