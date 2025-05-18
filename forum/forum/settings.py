1866ca3b50d5556e5817e635ddec04bbae9fb68e
Peter Liu <hyliu.amoiensis@gmail.com>
2025-05-18 21:05:57 +1200

    merged Tyler's work

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'forum_main',
     'user',
 ]
@@ -169,4 +170,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':  timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
